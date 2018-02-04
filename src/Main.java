571a16ec621d3abd4aaa26485d5e114b735ac31e
Kevin Kellar <kellar.kevin@gmail.com>
2018-02-03 16:36:12 -0800

    should chatroom2

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,42 @@ import java.net.Socket;
 
 public class Main
 {
-    public static void main (String[] args)
-    {
-        try {
-            ServerSocket ss = null;
+    public static void main (String[] args) throws IOException {
+//        try {
+//            ServerSocket ss = null;
+//
+//            ss = new ServerSocket(20000);
+//
+//            Socket s = null;
+//
+//            s= ss.accept();
+//
+//            PrintWriter writer;
+//
+//            writer = new PrintWriter(s.getOutputStream());
+//
+//            writer.println("HI BABE");
+//
+//            writer.flush();
+//
+//            System.out.println("?");
+//
+//
+//            Thread.sleep(5000);
+//
+//            System.out.println("??");
+//
+//            s.close();
+//
+//            ss.close();
+//        }
+//        catch (Exception ex)
+//        {
+//            ex.printStackTrace();
+//        }
 
-            ss = new ServerSocket(20000);
+        Server s = new Server(20000);
 
-            Socket s = null;
-
-            s= ss.accept();
-
-            PrintWriter writer;
-
-            writer = new PrintWriter(s.getOutputStream());
-
-            writer.println("HI BABE");
-
-            writer.flush();
-
-            System.out.println("?");
-
-
-            Thread.sleep(5000);
-
-            System.out.println("??");
-
-            s.close();
-
-            ss.close();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
 
 
     }
